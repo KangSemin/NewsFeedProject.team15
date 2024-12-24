85ff224cd7c6633877be6aff1b1b4bcc081c4cac
강세민 <semin15151@gmail.com>
2024-12-24 17:07:38 +0900

    feat : report
    유저 신고 기능 추가

---
+++ src/main/java/com/newsfeed/fakebook/repository/ReportRepository.java
@@ -0,0 +1,15 @@
+package com.newsfeed.fakebook.repository;
+
+import com.newsfeed.fakebook.domain.Report;
+import com.newsfeed.fakebook.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findByReporter(User reporter);
+    List<Report> findByReportedUser(User reportedUser);
+    List<Report> findByStatus(Report.ReportStatus status);
+} 
